18b9a247b5c5d490933f81037577ff5f4c394785
developerhr000 <80412774+developerhr000@users.noreply.github.com>
2022-04-09 18:15:05 +0600

    done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dicta
+        pariatur consequuntur recusandae repellat eum, quas harum accusamus rem
+        unde sequi aliquid nesciunt consequatur nisi exercitationem! Enim
+        debitis ratione reiciendis.
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlCu3G49_EVMRSRlKG-_RNHjMRbgg9SjQ",
+  authDomain: "router-firebase-integrat-b8adc.firebaseapp.com",
+  projectId: "router-firebase-integrat-b8adc",
+  storageBucket: "router-firebase-integrat-b8adc.appspot.com",
+  messagingSenderId: "69182519693",
+  appId: "1:69182519693:web:f289a880ec6145f64a25c4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
